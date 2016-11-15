dd334a7aad1e31039756239b1a7cd09a4d6cbd53
arturusfury <bri.reynolds@gmail.com>
2016-11-14 20:16:16 -0600

    added migration info for recipe model

--- db/migrate/20161115021152_create_recipes.rb
+++ db/migrate/20161115021152_create_recipes.rb
@@ -1,8 +1,13 @@
 class CreateRecipes < ActiveRecord::Migration[5.0]
   def change
     create_table :recipes do |t|
-
-      t.timestamps
+      t.string :title
+      t.integer :user_id
+      t.float :total_rating
+      t.integer :total_servings
+      t.string :description
+      t.string :image_url
+      t.string :dish_type
     end
   end
 end
