4f865d59323820582b1dc11c8b999f92d46032c4
arturusfury <bri.reynolds@gmail.com>
2016-11-14 21:37:56 -0600

    updated our recipe serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,17 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :image_url, :dish_type, :total_servings,
-             :total_rating, :user_id, :items
+             :total_rating, :user_id, :ingredient_list
+
+  def ingredient_list
+    items = object.items
+
+    items.map do |item|
+      {
+        id: item.id,
+        amount: item.amount,
+        measure: item.measure,
+        ingredient: Ingredient.find(item.ingredient_id)
+      }
+    end
+  end
 end
