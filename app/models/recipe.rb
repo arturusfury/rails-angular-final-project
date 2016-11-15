7d47504ddaeaef90e605482049410d436024dfc2
arturusfury <bri.reynolds@gmail.com>
2016-11-14 20:24:51 -0600

    started building associations for our models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  has_many :items
+  has_many :ingredients, through: :items
+  has_many :reviews
+  has_many :joins_recipe_tags
+  has_many :tags, through: :joins_recipe_tags
+  has_many :directions
 end
