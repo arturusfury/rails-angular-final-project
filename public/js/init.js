93da9538b7e6915c2ff716016b7519619a8d1f8d
Brian Reynolds <bri.reynolds@gmail.com>
2016-11-15 22:58:06 +0000

    added a few things to help standarize the sizes of our elments

--- public/js/init.js
+++ public/js/init.js
@@ -3,6 +3,11 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.recipe-card').matchHeight({
+      byRow: true,
+      property: 'height'
+    });
+    
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
