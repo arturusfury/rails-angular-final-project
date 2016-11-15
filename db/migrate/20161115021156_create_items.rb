7102b376dc3b7d9a878af3950a4a92d77547cc06
arturusfury <bri.reynolds@gmail.com>
2016-11-14 20:17:23 -0600

    added migration for items

--- db/migrate/20161115021156_create_items.rb
+++ db/migrate/20161115021156_create_items.rb
@@ -1,8 +1,10 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-
-      t.timestamps
+      t.float :amount
+      t.string :measure
+      t.integer :recipe_id
+      t.integer :ingredient_id
     end
   end
 end
