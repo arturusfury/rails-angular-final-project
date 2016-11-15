59d844a34a9e8dc0621304b521f600530c1c2b3b
arturusfury <bri.reynolds@gmail.com>
2016-11-15 09:56:57 -0600

    fixed our recipe_tags associations and model name

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   has_many :items
   has_many :ingredients, through: :items
   has_many :reviews
-  has_many :joins_recipe_tags
-  has_many :tags, through: :joins_recipe_tags
+  has_many :recipe_tags
+  has_many :tags, through: :recipe_tags
   has_many :directions
 end
