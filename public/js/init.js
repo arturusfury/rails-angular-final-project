13da02ad5c189a9800c846966f8fb33c0f3acee6
Brian Reynolds <bri.reynolds@gmail.com>
2016-11-18 03:16:02 +0000

    updated to add autocomplete testing

--- public/js/init.js
+++ public/js/init.js
@@ -3,10 +3,7 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    // $('.recipe-card').matchHeight({
-    //   byRow: false,
-    //   property: 'height'
-    // });
+    
     $('.card-content').matchHeight({
       byRow: false,
       property: 'height'
@@ -19,3 +16,32 @@
     
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+  $('select').material_select();
+  
+  $('#ingredient.autocomplete').autocomplete({
+    data: {
+      "New York Strip": null,
+      "Garlic": null,
+      "Soy Sauce": null,
+      "Worcestershire": null,
+      "Basil": null,
+    }
+  });
+  
+  $('#measure.autocomplete').autocomplete({
+    data: {
+      "Clove(s)": null,
+      "Tablespoon": null,
+      "Teaspoon": null,
+      "Cup": null,
+      "Pint": null,
+      "Gallon": null,
+      "Sprig": null,
+      "Pinch": null,
+      "Oz": null,
+      "Pound(s)": null
+    }
+  });
+});
