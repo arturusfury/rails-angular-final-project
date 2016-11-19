f58436f90fdbea376833bfedcf4f2652eb666bf8
arturusfury <bri.reynolds@gmail.com>
2016-11-18 22:12:25 -0600

    updated our recipe serializer to clean up our reviews object

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,7 +1,7 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :image_url, :dish_type, :total_servings,
              :prep_time, :cook_time, :total_rating, :user_info, :tags,
-             :ingredient_list, :directions, :reviews
+             :ingredient_list, :directions, :review_list
 
   def user_info
     user = object.user
@@ -21,6 +21,22 @@ class RecipeSerializer < ActiveModel::Serializer
         amount: item.amount,
         measure: item.measure,
         ingredient: Ingredient.find(item.ingredient_id)
+      }
+    end
+  end
+
+  def review_list
+    reviews = object.reviews
+
+    reviews.map do |review|
+      {
+        id: review.id,
+        user: {
+          id: review.user_id,
+          name: User.find(review.user_id).name
+        },
+        rating: review.rating,
+        content: review.content
       }
     end
   end
