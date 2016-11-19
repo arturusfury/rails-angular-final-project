f37742ae6b445c1eca18d6bb541a4c1e7c3c7b3c
arturusfury <bri.reynolds@gmail.com>
2016-11-18 21:52:32 -0600

    customized our user info in the recipe serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,7 +1,16 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :image_url, :dish_type, :total_servings,
-             :prep_time, :cook_time, :total_rating, :user, :tags, :ingredient_list,
-             :directions, :reviews
+             :prep_time, :cook_time, :total_rating, :user_info, :tags,
+             :ingredient_list, :directions, :reviews
+
+  def user_info
+    user = object.user
+
+    {
+      id: user.id,
+      name: user.name
+    }
+  end
 
   def ingredient_list
     items = object.items
