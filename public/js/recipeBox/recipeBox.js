283832b1d51575a3552fb986deb7a4ac7ca0ed29
arturusfury <bri.reynolds@gmail.com>
2016-11-18 21:50:17 -0600

    updated serializer for user information and got basic two way bindings working in some of our pages

--- public/js/recipeBox/recipeBox.js
+++ public/js/recipeBox/recipeBox.js
@@ -2,13 +2,12 @@ angular
   .module('recipeBox', ['ui.router', 'ui.materialize'])
   .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-  $urlRouterProvider.when('/', '/recipeBox').otherwise('/main');
+  $urlRouterProvider.otherwise('/main');
 
   $stateProvider
     .state('main', {
       url: '/',
       templateUrl: 'views/Main.controller.html'
-      // controller: 'MainController',
     })
     .state('main.list', {
       url: 'main',
@@ -16,37 +15,32 @@ angular
       controller: 'MainRecipesController',
       resolve: {
         recipes: function ($http) {
-          // return $http.get('http://localhost:3000/api/v1/recipes/')
+          return $http.get('http://localhost:3000/api/v1/recipes/')
         }
       }
     })
     .state('main.recipes', {
       url: 'recipes',
       templateUrl: 'views/recipes/Recipes.List.controller.html',
-      controller: 'RecipesListController',
+      controller: 'RecipesListController as recipes',
       resolve: {
         recipes: function ($http) {
-          // return $http.get('http://localhost:3000/api/v1/recipes/')
+          return $http.get('http://localhost:3000/api/v1/recipes/')
         }
       }
     })
     .state('main.recipe_add', {
       url: 'recipes/add',
       templateUrl: 'views/recipes/Recipe.Add.controller.html',
-      controller: 'RecipeAddController',
-      resolve: {
-        recipes: function ($http) {
-          // return $http.get('http://localhost:3000/api/v1/recipes/')
-        }
-      }
+      controller: 'RecipeAddController'
     })
     .state('main.recipe', {
       url: 'recipes/:id',
       templateUrl: 'views/recipes/Recipe.Details.controller.html',
-      controller: 'RecipeDetailsController',
+      controller: 'RecipeDetailsController as recipe',
       resolve: {
         recipe: function ($http, $stateParams) {
-          // return $http.get('http://localhost:3000/api/v1/recipes/' + $stateParams.id)
+          return $http.get('http://localhost:3000/api/v1/recipes/' + $stateParams.id)
         }
       }
     })
