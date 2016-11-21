2ac8400ea39e7c573ad85dd3d7a0790caf111810
arturusfury <bri.reynolds@gmail.com>
2016-11-21 15:38:30 -0600

    updated ingredient model to use name instead of the workd ingredient again

--- db/migrate/20161115021204_create_ingredients.rb
+++ db/migrate/20161115021204_create_ingredients.rb
@@ -3,7 +3,7 @@ class CreateIngredients < ActiveRecord::Migration[5.0]
     create_table :ingredients do |t|
       t.float :amount
       t.string :measure
-      t.string :ingredient
+      t.string :name
       t.integer :recipe_id
     end
   end
