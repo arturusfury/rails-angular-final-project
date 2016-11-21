b8b033008200f6ac6f3a97e4a57a7dda6aff189d
arturusfury <bri.reynolds@gmail.com>
2016-11-21 15:42:15 -0600

    started updating with ng-models

--- public/js/recipeBox/controllers/Recipe.Add.controller.js
+++ public/js/recipeBox/controllers/Recipe.Add.controller.js
@@ -1,24 +1,11 @@
 function RecipeAddController() {
-  $('#ingredient.autocomplete').autocomplete({
-    // $.get('/api/v1/ingredients', function(data, status) {
-    //   console.log(data);
-    // })
-  });
-
-  $('#measure.autocomplete').autocomplete({
-    data: {
-      "Clove(s)": null,
-      "Tablespoon": null,
-      "Teaspoon": null,
-      "Cup(s)": null,
-      "Pint": null,
-      "Gallon": null,
-      "Sprig": null,
-      "Pinch": null,
-      "Oz": null,
-      "Pound(s)": null
+  ctrl.recipe = {
+    ingredient_attributes: {
+      
     }
-  });
+  }
+
+
 }
 
 angular
