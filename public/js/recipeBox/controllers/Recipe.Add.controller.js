93cc52412b9831e8089ad048063f8ed974b96fd6
arturusfury <bri.reynolds@gmail.com>
2016-11-22 08:51:17 -0600

    worked on getting our ng-models into the recipe add page

--- public/js/recipeBox/controllers/Recipe.Add.controller.js
+++ public/js/recipeBox/controllers/Recipe.Add.controller.js
@@ -1,10 +1,26 @@
-function RecipeAddController() {
-  ctrl.recipe = {
-    ingredient_attributes: {
-      
-    }
-  }
+function RecipeAddController($scope) {
+  var ctrl = this;
 
+  ctrl.data = {
+    title: '',
+    prep_time: null,
+    cook_time: null,
+    total_servings: null,
+    description: '',
+    image_url: '',
+    ingredients: [
+      {
+        amount: null,
+        measure: '',
+        name: ''
+      }
+    ],
+    directions: [
+      {
+        text: ''
+      }
+    ]
+  }
 
 }
 
