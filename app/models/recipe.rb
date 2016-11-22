2ee36a2c738107b9b7c2f70284c05eebe52b0fd8
arturusfury <bri.reynolds@gmail.com>
2016-11-22 12:28:52 -0600

    worked on trying to get my nested attributes (ingredients, directions) working, no luck yet

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :ingredients
+  has_many :ingredients, inverse_of: :recipe
   has_many :reviews
   has_many :directions
+
+  accepts_nested_attributes_for :ingredients
+  accepts_nested_attributes_for :directions
 end
