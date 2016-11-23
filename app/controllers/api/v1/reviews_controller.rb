447f5c6b8d59418207f65ee9d6771f26489b4c84
arturusfury <bri.reynolds@gmail.com>
2016-11-23 09:53:17 -0600

    worked on getting our total reviews working correctly when a review has been created or deleted

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -4,20 +4,43 @@ class Api::V1::ReviewsController < ApplicationController
   def create
     review = Review.create(review_params)
 
-    render json: {
-      id: review.id,
-      user: {
-        id: review.user_id,
-        name: User.find(review.user_id).name
-      },
-      rating: review.rating,
-      content: review.content,
-      date: review.updated_at
-    }
+    ratings = Review.where(recipe_id: review.recipe_id).collect(&:rating)
+
+    recipe = Recipe.find(review.recipe_id)
+    recipe.update(total_rating: ratings.inject(0) { |a, e| a + e } / ratings.length)
+
+    if review.save
+      render json: {
+        status: 201,
+        message: 'Review successfully submitted!',
+        review: {
+          id: review.id,
+          user: {
+            id: review.user_id,
+            name: User.find(review.user_id).name
+          },
+          rating: review.rating,
+          content: review.content,
+          date: review.updated_at
+        }
+      }.to_json
+    else
+      render json: {
+        status: 500,
+        errors: review.errors
+      }.to_json
+    end
   end
 
   def destroy
-    Review.find(params[:id]).destroy
+    review = Review.find(params[:id])
+
+    ratings = Review.where(recipe_id: review.recipe_id).collect(&:rating)
+    recipe = Recipe.find(review.recipe_id)
+
+    review.destroy
+
+    recipe.update(total_rating: ratings.inject(0) { |a, e| a + e } / ratings.length)
   end
 
   private
