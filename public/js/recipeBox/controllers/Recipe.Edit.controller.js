cb29d1ef00d992a54fb1e12b6e43b62a6b1edbab
arturusfury <bri.reynolds@gmail.com>
2016-11-23 13:21:34 -0600

    cloned a bunch of code from our add controller into our edit controller to start with

--- public/js/recipeBox/controllers/Recipe.Edit.controller.js
+++ public/js/recipeBox/controllers/Recipe.Edit.controller.js
@@ -1,5 +1,81 @@
 function RecipeEditController() {
+  var ctrl = this;
 
+  // Create a blank set of information for our recipe
+  ctrl.recipe = {
+    title: '',
+    prep_time: null,
+    cook_time: null,
+    total_servings: null,
+    total_rating: 0,
+    dish_type: 'Main Course',
+    description: '',
+    image_url: '',
+    user_id: $rootScope.user.id,
+    ingredients_attributes: [
+      {
+        amount: null,
+        measure: '',
+        name: ''
+      }
+    ],
+    directions_attributes: [
+      {
+        text: ''
+      }
+    ]
+  }
+
+  ctrl.dishTypes = [
+    'Appitizers & Snacks',
+    'Breads, Muffins, Scones & Pancakes',
+    'Breakfast',
+    'Desert',
+    'Dinner',
+    'Dips, Salad Dressings, Salsas, and Sauces',
+    'Drinks',
+    'Lunch',
+    'Main Course',
+    'One Pot Meal',
+    'Salads',
+    'Side Dishes',
+    'Snacks',
+    'Soups, Stews & Chili'
+  ]
+
+  ctrl.addIngredient = function () {
+    ctrl.recipe.ingredients_attributes.push({
+      amount: null,
+      measure: '',
+      name: ''
+    })
+  }
+
+  ctrl.deleteIngredient = function (index) {
+    console.log("Delete: " + index)
+    ctrl.recipe.ingredients_attributes.splice(index, 1);
+  }
+
+  ctrl.addDirection = function () {
+    ctrl.recipe.directions_attributes.push({
+      text: ''
+    })
+  }
+
+  ctrl.deleteDirection = function (index) {
+    console.log("Delete: " + index)
+    ctrl.recipe.directions_attributes.splice(index, 1);
+  }
+
+  ctrl.submitForm = function () {
+    $http.put('/api/v1/recipes/', {
+      recipe: ctrl.recipe
+    }).success(function(data, status, headers, config) {
+      $location.path('/recipes/' + data.recipe.id);
+    }).error(function(data, status, headers, config) {
+      console.log('Failure: ' + JSON.stringify({data: data}));
+    });
+  }
 }
 
 angular
