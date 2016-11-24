f0e485133f7b0297f8b25a07766493276fe01065
arturusfury <bri.reynolds@gmail.com>
2016-11-23 18:33:05 -0600

    added a new directive for our search bar to handle pressing enter

---
+++ public/js/recipeBox/directives/OnKeyEnter.directive.js
@@ -0,0 +1,15 @@
+angular.module('recipeBox')
+    .directive('onKeyEnter', OnKeyEnter);
+
+function OnKeyEnter() {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.onKeyEnter);
+                });
+                event.preventDefault();
+            }
+        });
+    };
+}
