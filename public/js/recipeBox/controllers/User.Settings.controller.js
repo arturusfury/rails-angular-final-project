db8cd30e7bcb045099f2321ee3b09477fea90f16
arturusfury <bri.reynolds@gmail.com>
2016-11-28 08:26:57 -0600

    worked on our password change, ran into a rails issue with cookie size limits, more research is needed.  commented it out for now

--- public/js/recipeBox/controllers/User.Settings.controller.js
+++ public/js/recipeBox/controllers/User.Settings.controller.js
@@ -1,21 +1,22 @@
-function UserSettingsController($scope, $location, $http, $rootScope) {
+function UserSettingsController($scope, $location, $http, $auth) {
   var ctrl = this;
 
   ctrl.confirmDelete = false;
   ctrl.isDisabled = false;
-  ctrl.passwordChangeSuccess = false;
-  ctrl.passwordChangeFails = false;
-
-  ctrl.changePassword = function () {
-    $auth.updatePassword({
-      password: ctrl.password,
-      password_confirmation: ctrl.passwordConfirmation
-    }).then(function (resp) {
-      // success
-    }).catch(function (resp) {
-      $scope.errors = resp.data.errors.full_messages;
-    });
-  }
+  // ctrl.passwordChangeSuccess = false;
+  //
+  // ctrl.changePassword = function () {
+  //   $auth.updatePassword({
+  //     password: ctrl.password,
+  //     password_confirmation: ctrl.passwordConfirmation
+  //   }).then(function (resp) {
+  //     ctrl.password = "";
+  //     ctrl.passwordConfirmation = "";
+  //     ctrl.passwordChangeSuccess = true;
+  //   }).catch(function (resp) {
+  //     console.log(resp);
+  //   });
+  // }
 
   ctrl.deleteConfirmation = function () {
     ctrl.confirmDelete = true;
