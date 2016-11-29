3e48c83c0d907524a7c923ecef57d3090fc1282a
arturusfury <bri.reynolds@gmail.com>
2016-11-29 14:12:33 -0600

    updated our application controller and added a users controller, that one might not stay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
-# require 'application_responder'
+# Main Application Controller
 class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :set_csrf_cookie_for_ng
+  respond_to :json
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 
   protected
 
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :format, :session])
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :username) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :username, :admin) }
   end
 end
