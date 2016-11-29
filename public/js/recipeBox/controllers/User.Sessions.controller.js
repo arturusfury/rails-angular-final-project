2b3dfa83ed00d457e21d084369e73b0c2cfdad80
arturusfury <bri.reynolds@gmail.com>
2016-11-29 15:07:55 -0600

    added a forced login using set information, now to get our form working

--- public/js/recipeBox/controllers/User.Sessions.controller.js
+++ public/js/recipeBox/controllers/User.Sessions.controller.js
@@ -1,6 +1,27 @@
-function UserSessionsController($scope) {
-  $scope.$on('auth:login-error', function(ev, reason) {
-    $scope.error = reason.errors[0];
+function UserSessionsController($scope, Auth) {
+  var credentials = {
+    email: 'tthomas@gmail.com',
+    password: 'password'
+  };
+
+  var config = {
+    headers: {
+      'X-HTTP-Method-Override': 'POST'
+    }
+  }
+
+  Auth.login(credentials, config).then(function(user) {
+    console.log(user);
+  }, function(error) {
+    console.log(error);
+  });
+
+  $scope.$on('devise:login', function(event, currentUser) {
+    // after a login, a hard refresh, a new tab
+  });
+
+  $scope.$on('devise:new-session', function(event, currentUser) {
+    // user logged in by Auth.login({...})
   });
 }
 
