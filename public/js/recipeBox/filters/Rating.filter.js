cdc920ffe7a9ad5c47678945e073c8733b2ebc7d
arturusfury <bri.reynolds@gmail.com>
2016-11-28 19:11:09 -0600

    got our rating filter working correctly as far as I can tell

--- public/js/recipeBox/filters/Rating.filter.js
+++ public/js/recipeBox/filters/Rating.filter.js
@@ -6,9 +6,19 @@ angular.module('recipeBox')
         }
         var result = [];
         angular.forEach(arr, function(item){
-          if(item.total_rating == rating){
+          if (rating == "") {
             result.push(item);
-          }
+          } else {
+            if (rating == "1" && item.total_rating <= 2) {
+              result.push(item);
+            } else if (rating == "2" && item.total_rating >= 2 && item.total_rating < 3) {
+              result.push(item);
+            } else if (rating == "3" && item.total_rating >= 3 && item.total_rating < 4) {
+              result.push(item);
+            } else if (rating == "4" && item.total_rating >= 4 && item.total_rating < 5) {
+              result.push(item);
+            }
+          };
         });
         return result;
     };
