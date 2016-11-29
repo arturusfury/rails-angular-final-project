c1611177c088af41e59696e8dfe0622f647feac0
arturusfury <bri.reynolds@gmail.com>
2016-11-28 19:16:32 -0600

    got a servings size filter working on our list of recipes

--- public/js/recipeBox/filters/Servings.filter.js
+++ public/js/recipeBox/filters/Servings.filter.js
@@ -6,8 +6,16 @@ angular.module('recipeBox')
         }
         var result = [];
         angular.forEach(arr, function(item){
-          if(item.total_servings == servings){
+          if (servings == "") {
             result.push(item);
+          } else {
+            if (servings == "2" && item.total_servings <= 2) {
+              result.push(item);
+            } else if (servings == "3 - 8" && item.total_servings > 2 && item.total_servings < 9) {
+              result.push(item);
+            } else if (servings == "9+" && item.total_servings > 8) {
+              result.push(item);
+            }
           }
         });
         return result;
