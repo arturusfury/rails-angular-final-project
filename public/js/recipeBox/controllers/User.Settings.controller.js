2832ecfd3e405f16e4bb7d3e8b54fb37c08d7e37
arturusfury <bri.reynolds@gmail.com>
2016-11-30 11:09:24 -0600

    added our password change information back in, trying to get it functional

--- public/js/recipeBox/controllers/User.Settings.controller.js
+++ public/js/recipeBox/controllers/User.Settings.controller.js
@@ -1,22 +1,24 @@
-function UserSettingsController($scope, $location, $http) {
+function UserSettingsController($location, $http, $rootScope) {
   var ctrl = this;
 
   ctrl.confirmDelete = false;
   ctrl.isDisabled = false;
-  // ctrl.passwordChangeSuccess = false;
-  //
-  // ctrl.changePassword = function () {
-  //   $auth.updatePassword({
-  //     password: ctrl.password,
-  //     password_confirmation: ctrl.passwordConfirmation
-  //   }).then(function (resp) {
-  //     ctrl.password = "";
-  //     ctrl.passwordConfirmation = "";
-  //     ctrl.passwordChangeSuccess = true;
-  //   }).catch(function (resp) {
-  //     console.log(resp);
-  //   });
-  // }
+  ctrl.passwordChangeSuccess = false;
+
+  ctrl.changePassword = function () {
+    $http.put('/users/',{
+      email: $rootScope.user.email,
+      username: $rootScope.user.username,
+      password: ctrl.password,
+      password_confirmation: ctrl.passwordConfirmation
+    }).then(function (resp) {
+      ctrl.password = "";
+      ctrl.passwordConfirmation = "";
+      ctrl.passwordChangeSuccess = true;
+    }).catch(function (resp) {
+      console.log(resp);
+    });
+  }
 
   ctrl.deleteConfirmation = function () {
     ctrl.confirmDelete = true;
@@ -37,6 +39,12 @@ function UserSettingsController($scope, $location, $http) {
     });
 
     $http.delete('/users/', data)
+         .then( function(data) {
+           console.log(data)
+         }, function (error) {
+           console.log(data)
+         });
+    $rootScope.user = {};
     $location.path('/');
   }
 }
