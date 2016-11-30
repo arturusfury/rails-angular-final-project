7a496bcef3f1c3743b0b5a9903da88bd72dc1df4
arturusfury <bri.reynolds@gmail.com>
2016-11-30 14:04:13 -0600

    cleaned up tests a little

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,28 +1,34 @@
 RSpec.describe User, type: :model do
-  it { should respond_to :name }
-  it do
-    should validate_presence_of(:name)
-      .with_message('Must have a full name')
+  describe 'Full Name' do
+    it { should respond_to :name }
+    it do
+      should validate_presence_of(:name)
+        .with_message('Must have a full name')
+    end
   end
 
-  it { should respond_to :email }
-  it do
-    should validate_presence_of(:email)
-      .with_message('Must have a valid Email Address')
-  end
-  it do
-    should validate_uniqueness_of(:email)
-      .with_message('Email Address is already in use')
-      .case_insensitive
+  describe 'Email' do
+    it { should respond_to :email }
+    it do
+      should validate_presence_of(:email)
+        .with_message('Must have a valid Email Address')
+    end
+    it do
+      should validate_uniqueness_of(:email)
+        .with_message('Email Address is already in use')
+        .case_insensitive
+    end
   end
 
-  it { should respond_to :username }
-  it do
-    should validate_presence_of(:username)
-      .with_message('Must have a valid username')
-  end
-  it do
-    should validate_uniqueness_of(:username)
-      .with_message('Username already exists')
+  describe 'Username' do
+    it { should respond_to :username }
+    it do
+      should validate_presence_of(:username)
+        .with_message('Must have a valid username')
+    end
+    it do
+      should validate_uniqueness_of(:username)
+        .with_message('Username already exists')
+    end
   end
 end
