7483bd87b6818522a2d6c331bcb50d6822b76c24
arturusfury <bri.reynolds@gmail.com>
2016-11-30 13:06:22 -0600

    added in a password controller to send json back

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, controllers: {sessions: 'sessions', registrations: 'registrations'}
+  devise_for :users, controllers: {sessions: 'sessions', registrations: 'registrations', passwords: 'passwords'}, defaults: { format: :json }
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
