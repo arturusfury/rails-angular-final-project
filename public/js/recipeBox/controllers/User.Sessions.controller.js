1cd326f868595d4d5f19791ef38e7ecdcd05867e
arturusfury <bri.reynolds@gmail.com>
2016-11-29 18:03:31 -0600

    fixed the controller as for our registration controller and removed old login code

--- public/js/recipeBox/controllers/User.Sessions.controller.js
+++ public/js/recipeBox/controllers/User.Sessions.controller.js
@@ -9,29 +9,6 @@ function UserSessionsController($rootScope, Auth) {
         console.log(error);
     });
   };
-  // var ctrl = this;
-  //
-  // ctrl.submitLogin = function (credentials) {
-  //   var config = {
-  //     headers: {
-  //       'X-HTTP-Method-Override': 'POST'
-  //     }
-  //   }
-  //
-  //   Auth.login(credentials, config).then(function(user) {
-  //     console.log(user);
-  //   }, function(error) {
-  //     console.log(error);
-  //   });
-  // }
-  //
-  // $scope.$on('devise:login', function(event, currentUser) {
-  //   // after a login, a hard refresh, a new tab
-  // });
-  //
-  // $scope.$on('devise:new-session', function(event, currentUser) {
-  //   // user logged in by Auth.login({...})
-  // });
 }
 
 angular
