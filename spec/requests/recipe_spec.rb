ed6459a5234e7e1edb0827d4e2b871eb46748370
arturusfury <bri.reynolds@gmail.com>
2016-11-30 19:53:32 -0600

    worked on some more tests for the basic api functions

--- spec/requests/recipe_spec.rb
+++ spec/requests/recipe_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
-  before(:each) do
-    user = FactoryGirl.create :user
-    FactoryGirl.create :prime_rib, user_id: user.id
-    FactoryGirl.create :chicken_wings, user_id: user.id
-  end
+  let(:user) { FactoryGirl.create :tim }
 
   describe 'GET /recipes' do
     it 'returns all of our recipes' do
+      FactoryGirl.create :prime_rib, user_id: user.id
+      FactoryGirl.create :chicken_wings, user_id: user.id
+
       get '/api/v1/recipes'
 
       expect(response.status).to eq 200
@@ -26,6 +25,9 @@ RSpec.describe 'Recipes', type: :request do
 
   describe 'GET /recipes/:id' do
     it 'should return the specified recipe' do
+      FactoryGirl.create :prime_rib, user_id: user.id
+      FactoryGirl.create :chicken_wings, user_id: user.id
+
       get '/api/v1/recipes/3'
 
       expect(response.status).to eq 200
@@ -34,6 +36,49 @@ RSpec.describe 'Recipes', type: :request do
       recipe_title = body['title']
       expect(recipe_title).to eq('Grilled Prime Rib')
       expect(recipe_title).to_not eq('Baked Chicken Wings')
+    end
+  end
+
+  describe 'POST /recipes' do
+    before do
+      recipe = {
+        recipe: {
+          user_id: user.id,
+          title: 'Grilled Prime Rib',
+          description: 'This is a classic prime rib recipe cooked on the grill. The secret is to keep the temperature low enough during cooking and to remove it immediately from the grill once done. Be sure to grill indirectly and also use a drip pan to catch those delicious drippings.',
+          image_url: 'http://f.tqn.com/y/bbq/1/W/k/H/1/GettyImages-183369518.jpg',
+          dish_type: 'Main Course',
+          total_servings: 14,
+          prep_time: 20,
+          cook_time: 180,
+          total_rating: 1.2
+        }
+      }
+
+      post '/api/v1/recipes',
+        params: recipe.to_json,
+        headers: { 'Content-Type': 'application/json' }
+
+      @body = JSON.parse(response.body)
+    end
+
+    it 'creates a new recipe' do
+      expect(response.status).to eq 201
+    end
+
+    describe 'after creation it' do
+      it 'should have the correct title' do
+        expect(@body['recipe']['title']).to eq('Grilled Prime Rib')
+      end
+
+      it 'should contain the correct cook/prep times' do
+        expect(@body['recipe']['prep_time']).to eq(20)
+        expect(@body['recipe']['cook_time']).to eq(180)
+      end
+
+      it 'should belong to a user' do
+        expect(@body['recipe']['user_id']).to eq(user.id)
+      end
     end
   end
 end
