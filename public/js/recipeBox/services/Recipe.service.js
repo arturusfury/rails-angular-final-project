7e2ad448c8049001523e99380fdb244c80309a79
arturusfury <bri.reynolds@gmail.com>
2016-12-01 08:51:45 -0600

    looked into adding a recipe service as well

---
+++ public/js/recipeBox/services/Recipe.service.js
@@ -0,0 +1,10 @@
+angular.module('recipeBox')
+  .service('Recipe', ['$http', function ($http) {
+    this.get = function(onSuccess,onError) {
+
+    }
+
+    this.create = function(onSuccess,onError) {
+
+    }
+  }]);
