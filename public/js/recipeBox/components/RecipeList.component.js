b6238f1e9f2f1ed656088af3b907955a6061a7fe
arturusfury <bri.reynolds@gmail.com>
2016-12-15 11:54:48 -0600

    added a controller to our component, doesn't have much content yet

--- public/js/recipeBox/components/RecipeList.component.js
+++ public/js/recipeBox/components/RecipeList.component.js
@@ -3,6 +3,13 @@ var RecipeListComponent = {
     heading: '<heading',
   },
   templateUrl: 'views/components/RecipeList.template.html',
+  controller: RecipeListController
+}
+
+function RecipeListController(RecipeService) {
+  var ctrl = this;
+
+  ctrl.recipes = RecipeService.getTopRecipes;
 }
 
 angular.module('recipeBox')
