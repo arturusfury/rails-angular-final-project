26b45474f7007ed5e92294dfa3f44ae09795d990
arturusfury <bri.reynolds@gmail.com>
2016-12-16 14:06:22 -0600

    fixed a minor issue with our serving filter, it wasn't actually filtering the 3 - 8 serving range correctly

--- public/js/recipeBox/filters/Servings.filter.js
+++ public/js/recipeBox/filters/Servings.filter.js
@@ -11,7 +11,7 @@ angular.module('recipeBox')
           } else {
             if (servings == "2" && item.total_servings <= 2) {
               result.push(item);
-            } else if (servings == "3 - 8" && item.total_servings > 2 && item.total_servings < 9) {
+            } else if (servings == "3-8" && item.total_servings > 2 && item.total_servings < 9) {
               result.push(item);
             } else if (servings == "9+" && item.total_servings > 8) {
               result.push(item);
