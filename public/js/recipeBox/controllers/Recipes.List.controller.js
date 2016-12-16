e322dbbb8f83596e68ad03d377c0225c05d47c60
arturusfury <bri.reynolds@gmail.com>
2016-12-16 07:54:37 -0600

    worked on adding a new component for our dish type select box, it's displaying correctly but not updating

--- public/js/recipeBox/controllers/Recipes.List.controller.js
+++ public/js/recipeBox/controllers/Recipes.List.controller.js
@@ -2,23 +2,6 @@ function RecipesListController(recipes) {
   var ctrl = this;
 
   ctrl.recipes = recipes.data
-
-  ctrl.dishTypes = [
-    'Appitizers & Snacks',
-    'Breads, Muffins, Scones & Pancakes',
-    'Breakfast',
-    'Desert',
-    'Dinner',
-    'Dips, Salad Dressings, Salsas, and Sauces',
-    'Drinks',
-    'Lunch',
-    'Main Course',
-    'One Pot Meal',
-    'Salads',
-    'Side Dishes',
-    'Snacks',
-    'Soups, Stews & Chili'
-  ]
 }
 
 angular
