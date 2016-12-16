50b9080fbd31fe5db413c2fdfea40dfcfd1082ee
arturusfury <bri.reynolds@gmail.com>
2016-12-15 18:55:48 -0600

    added the ability to have a default image loaded if we dont use src

--- public/js/recipeBox/components/ImageBanner.component.js
+++ public/js/recipeBox/components/ImageBanner.component.js
@@ -1,8 +1,13 @@
-var imageBanner = {
+const imageBanner = {
   bindings: {
     imageUrl: '@src'
   },
-  templateUrl: 'views/components/ImageBanner.template.html'
+  templateUrl: 'views/components/ImageBanner.template.html',
+  controller: function () {
+    var ctrl = this;
+
+    if (ctrl.imageUrl == null) ctrl.imageUrl = 'img/backgrounds/03.jpg'
+  }
 }
 
 angular.module('recipeBox')
