fcadc01672669c60c4708c3626a7cad26a4b1ade
arturusfury <bri.reynolds@gmail.com>
2016-12-16 13:58:01 -0600

    removed another old controller that got migrated to our components

--- public/js/recipeBox/controllers/Recipes.List.controller.js
+++
@@ -1,9 +0,0 @@
-function RecipesListController(recipes) {
-  var ctrl = this;
-
-  ctrl.recipes = recipes.data
-}
-
-angular
-  .module('recipeBox')
-  .controller('RecipesListController', RecipesListController)
